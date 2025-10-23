cbf987ff7fe5bdc288e6267422d322df8a045a44
Prynnce <armobernieprince@gmail.com>
2025-10-23 16:57:56 +0000

    updated validator.py
    Update loose_threshold value in CONFIG from 60 to 90

--- validator.py
+++ validator.py
@@ -4,7 +4,7 @@ from rapidfuzz import fuzz, process
 # --- Global Settings .i.e. configurable parameters---
 CONFIG = {
     'strict_threshold': 70,
-    'loose_threshold': 60,
+    'loose_threshold': 90,
     'min_salary': 539,
     'max_salary': 61000,
     'contribution_tolerance': 0.5
